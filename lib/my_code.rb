3374376a6a7719a05e1b2ac6b4515d0c231dd95c
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-09 14:27:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
-# My Code here....
+  #describe 'my own map-like methods' do
+  #describe "map_to_negativize returns an array with all values made negative" do
+  #  it "transforms correctly" do
+  #    expect(map_to_negativize([1, 2, 3, -9])).to eq([-1, -2, -3, 9])
+  #  end
+  #end
+  # My Code here....
+def map_to_negativize 
+  map_to_negativize = [1, 2, 3, -9]
+end
