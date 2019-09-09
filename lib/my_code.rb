390bd0c7b206737ddb3381a14512556ff788a5d5
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-09 14:33:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,12 +15,3 @@ def map_to_negativize(array)
   return new
 end 
 
-map_to_no_change(array)
-new = []
-  i = 0 
-  while i < array.length do
-    new.push( array[i] )
-    i += 1
-  end
-  return new
-end
