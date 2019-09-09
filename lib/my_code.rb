2556e7a914ca286fcbb065e32444688dc3c06a58
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-09-09 14:38:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,5 +39,8 @@ def map_to_square(array)
   new = []
   i = 0 
   while i < array.length do
-    new.push(array[i] math.sqrt)
-    i +=
+    new.push(array[i] * array[i])
+    i += 1 
+  end 
+  return new
+end
